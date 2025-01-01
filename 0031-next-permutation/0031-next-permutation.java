226446fd209c65e512f9a774ad2e87bbd8cdbbf1
Aditya_Gund <73999596+aditya-gund@users.noreply.github.com>
2025-01-01 14:03:15 +0530

    Time: 0 ms (100%), Space: 43.2 MB (27.69%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,35 +1,45 @@
 class Solution {
-    private void reverse(int[] nums, int start, int end){
-        while(start<end){
-            int temp=nums[start];
-            nums[start]=nums[end];
-            nums[end]=temp;
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
             start++;
             end--;
         }
-        
     }
+
     public void nextPermutation(int[] nums) {
-        int n=nums.length;
-        int index=-1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                index=i;
+        int n = nums.length;
+        int index = -1;
+
+        // Step 1: Find the first decreasing element from the end
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
                 break;
             }
         }
-              if(index==-1){
-                reverse(nums,0,n-1);
-                return;
-            }
-        for(int i=n-1;i>index;i--){
-            if(nums[i]>nums[index]){
-                int temp=nums[i];
-                nums[i]=nums[index];
-                nums[index]=temp;
-                break;
+
+        // Step 2: If no decreasing element is found, reverse the array
+        if (index == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        // Step 3: Find the next larger element and swap
+        for (int i = n - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums, i, index);
+                break; // Exit after swapping
             }
         }
-         reverse(nums, index+1, n-1);
+
+        // Step 4: Reverse the part of the array after the swapped element
+        reverse(nums, index + 1, n - 1);
     }
 }
