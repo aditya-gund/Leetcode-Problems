dddb68a0cafcf0b3a01c8e94de66231f7f44283b
Aditya_Gund <73999596+aditya-gund@users.noreply.github.com>
2025-02-10 14:04:06 +0530

    Time: 1 ms (99.74%), Space: 43.5 MB (77.48%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -2,18 +2,17 @@ class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        generateAllPossibleSubset(nums,0, new ArrayList<>(), ans);
+        generateSubsets(nums, 0, new ArrayList<>(), ans);
         return ans;
     }
-    private void generateAllPossibleSubset(int[] arr, int index, List<Integer> curr, List<List<Integer>>ans){
-        ans.add(new ArrayList<>(curr));
-        // Skip duplicate sets
+    private void generateSubsets(int[] arr, int index, List<Integer>curr, List<List<Integer>> result){
+        result.add(new ArrayList<>(curr));
         for(int i=index;i<arr.length;i++){
             if(i>index && arr[i]==arr[i-1]){
                 continue;
             }
             curr.add(arr[i]);
-            generateAllPossibleSubset(arr, i+1, curr, ans);
+            generateSubsets(arr, i+1, curr,result);
             curr.remove(curr.size()-1);
         }
     }
