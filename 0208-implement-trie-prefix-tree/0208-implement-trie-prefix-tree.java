960440b2a9008a115b282b8d8e72abd6680dc213
Aditya_Gund <73999596+aditya-gund@users.noreply.github.com>
2025-03-23 14:56:18 +0530

    Time: 40 ms (35.37%), Space: 55.8 MB (26.54%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,31 +1,30 @@
 class Trie {
     class Node{
-        Node[] links = new Node[26];
-        boolean flag=false;
-        boolean containsKey(char ch){
-            return links[ch-'a']!=null;
-        }
-        void put(char ch, Node node){
-            links[ch-'a']=node;
-        }
-        Node get(char ch){
-            return links[ch-'a'];
-        }
-        void setEnd(){
-            flag=true;
-        }
-        boolean isEnd(){
-            return flag;
-        }
+    Node[] links = new Node[26];
+    boolean flag=false;
+    boolean containsKey(char ch){
+        return links[ch-'a']!=null;
     }
-     private Node root;
+    void put(char ch, Node node){
+        links[ch-'a']=node ;
+    }
+    Node get(char ch){
+        return links[ch-'a'];
+    }
+    void setEnd(){
+        flag=true;
+    }
+    boolean isEnd(){
+        return flag;
+    }
+    }
+    private Node root;
     public Trie() {
-         root = new Node();
-        
+        root = new Node();
     }
     
     public void insert(String word) {
-        Node node=root;
+        Node node = root;
         for(int i=0;i<word.length();i++){
             if(!node.containsKey(word.charAt(i))){
                 node.put(word.charAt(i), new Node());
@@ -47,7 +46,7 @@ class Trie {
     }
     
     public boolean startsWith(String prefix) {
-        Node node=root;
+        Node node = root;
         for(int i=0;i<prefix.length();i++){
             if(!node.containsKey(prefix.charAt(i))){
                 return false;
