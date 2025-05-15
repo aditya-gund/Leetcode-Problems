07890b49578f6778886bc6c852dd34d3a5bbe63c
Aditya_Gund <73999596+aditya-gund@users.noreply.github.com>
2025-05-15 09:25:16 +0530

    Time: 25 ms (68.31%), Space: 42.9 MB (54.46%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -20,16 +20,20 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
-        return dfs(node,new HashMap<>());
+        return dfs(node, new HashMap<>());
     }
+    // Implementation of the DFS Methos using HashMap
     private Node dfs(Node node, Map<Node,Node> visited){
-        // Base Condition
+        // Base condition
         if(node == null) return null;
+        // Check if node is visited or not 
         if(visited.containsKey(node)) return visited.get(node);
+        // Intialize the Clone Node object
         Node clone = new Node(node.val);
+        // Insert the Original node and clone Node into the map
         visited.put(node,clone);
-        for(Node n: node.neighbors){
-            clone.neighbors.add(dfs(n,visited));
+        for(Node neighbor: node.neighbors){
+                clone.neighbors.add(dfs(neighbor,visited));
         }
         return clone;
     }
