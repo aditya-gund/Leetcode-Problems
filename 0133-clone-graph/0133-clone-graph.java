44a4edbbd4b1d46946fdbe5b91ba3599a756d3e5
Aditya_Gund <73999596+aditya-gund@users.noreply.github.com>
2025-05-27 12:02:41 +0530

    Time: 25 ms (66.59%), Space: 43.3 MB (7.05%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -20,18 +20,22 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
+        // Call recursive method
         return dfs(node, new HashMap<>());
     }
+    // Recursive Method DFS
     private Node dfs(Node node, Map<Node,Node> visited){
-        if(node==null) return null;
-        if(visited.containsKey(node)) return visited.get(node);
-         Node clone = new Node(node.val);
-            visited.put(node, clone);
+        // Base condition
+        if(node== null) return null;
+        if(visited.containsKey(node)){
+            return visited.get(node);
+        }
+        // Intialize clone node
+        Node clone = new Node(node.val);
+        visited.put(node,clone);
         for(Node neighbor: node.neighbors){
-    clone.neighbors.add(dfs(neighbor, visited));
-    
+                clone.neighbors.add(dfs(neighbor, visited));
+        }
+        return clone;
     }
-    
-    return clone;
-}   
 }
